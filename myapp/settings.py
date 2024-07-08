30ea12fa29a712e3667d3966cd52752724b79035
Tony <tonykbl@yahoo.co.uk>
2024-07-08 02:22:44 +0100

    Update settings.py

--- myapp/settings.py
+++ myapp/settings.py
@@ -27,7 +27,7 @@ PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.SECRET_KEY
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
