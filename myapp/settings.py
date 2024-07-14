97c2717ea2c6cc7b32afb33734e9f5d3fc7b03e9
Tony <tonykbl@yahoo.co.uk>
2024-07-14 07:45:50 +0100

    v

--- myapp/settings.py
+++ myapp/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 # from pathlib import Path
 # import os
-# import .env
+# import env
 
 from django.core.management.utils import get_random_secret_key
 from pathlib import Path
@@ -25,6 +25,7 @@ from urllib.parse import urlparse
 # Initialise environment variables
 
 DEVELOPMENT_MODE = "True"
+USE_AWS = "True"
 # from .aws.conf import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
