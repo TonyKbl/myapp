7d9d1696f0e053384ea67cb63d9305258e229ddc
Tony <tonykbl@yahoo.co.uk>
2024-08-20 01:55:20 +0100

    Minor bug fixes

--- pages/templatetags/custom_page_filters.py
+++ pages/templatetags/custom_page_filters.py
@@ -1,5 +1,5 @@
 from django import template
-from django.db.models import Avg
+from django.db.models import Avg, Count
 from ..models import PageFollow, PageReviews
 
 register = template.Library()
@@ -11,6 +11,17 @@ def is_following_page(user, target_page):
 @register.simple_tag
 def avg_rating(target_page):
     # return PageReviews.objects.filter(page_name=target_page).values()[0]
-    average = PageReviews.objects.filter(page_name=target_page).aggregate(Avg('rating')).values()
-    return average
+    average = PageReviews.objects.filter(page_name=target_page).aggregate(Avg('rating'))
+    if average['rating__avg'] == None:
+        return 0
+    else:
+        print(average)
+        return round(average['rating__avg'], 2)
+    # return PageReviews.objects.aggregate(Avg('rating'))
+
+@register.simple_tag
+def review_count(target_page):
+    # return PageReviews.objects.filter(page_name=target_page).values()[0]
+    count = PageReviews.objects.filter(page_name=target_page).aggregate(Count('rating'))
+    return count['rating__count']
     # return PageReviews.objects.aggregate(Avg('rating'))
