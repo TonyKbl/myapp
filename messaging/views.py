d9deced0f58f8ccca1356730a0d95262e812ce8f
Tony <tonykbl@yahoo.co.uk>
2024-09-18 21:05:22 +0100

    Update views.py

--- messaging/views.py
+++ messaging/views.py
@@ -22,8 +22,8 @@ class MessageInboxView(LoginRequiredMixin, ListView):
 
  
     def get_queryset(self):
-        qs1 = Message.objects.filter(msg_to=self.request.user).distinct('msg_from')
-        qs2 = qs1.filter(msg_to=self.request.user).order_by('-date_sent')
+        qs1 = Message.objects.filter(msg_to=self.request.user).order_by('-date_sent')
+        qs2 = qs1.filter(msg_to=self.request.user).distinct('msg_from')
         # qs2 = qs1.order_by('msg_from', '-date_sent').distinct('msg_from').order_by('-date_sent')
         # return qs2
         return qs2
