4956f9f094d45a41b36af3737f0308980f0b039a
Tony <tonykbl@yahoo.co.uk>
2024-09-25 00:48:17 +0100

    Event urls modified

--- events/urls.py
+++ events/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = "events"
 
 urlpatterns = [
-    path("events/", views.EventsList.as_view(), name="events"),
+    path("events.html", views.EventsList.as_view(), name="events"),
     path("events/<int:pk>", views.PageEventsList.as_view(), name="page_events"),
     path("add_event/<int:pk>", views.PageAddEventView.as_view(), name="add_event"),
     path("add_date/<int:pk>", views.AddEventDateView.as_view(), name="add_date"),
-    path("event/<int:pk>/", views.EventDetailView.as_view(), name="event_detail"),  
+    path("swinger-club-event/<int:pk>/<str:club>/<str:date>/<str:title>.html", views.EventDetailView.as_view(), name="event_detail"),  
     ]
