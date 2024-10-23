6180ab0b1d56baf67991833180b6d11e442a8fe2
Tony <tonykbl@yahoo.co.uk>
2024-10-23 06:29:21 +0100

    minor bug fix

--- profiles/views.py
+++ profiles/views.py
@@ -106,8 +106,8 @@ class InterestUpdateView(LoginRequiredMixin, UpdateView):
     def get_success_url(self):
         return reverse("profiles:detail", kwargs={"username": self.request.user})
 
-    # def get_object(self):
-    #    return self.request.user
+    def get_object(self):
+        return self.request.user
 
     # def get_object(self, queryset=None):
     #     obj = Interest.objects.get(user=self.request.user)
