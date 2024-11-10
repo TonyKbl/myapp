8e3c53ffc184742e35de20f1dd64543eb9388a6a
Tony <tonykbl@yahoo.co.uk>
2024-11-10 09:42:49 +0000

    landing

--- feed/urls.py
+++ feed/urls.py
@@ -1,12 +1,22 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 app_name = "feed"
 
 urlpatterns = [
     path("", views.HomePage.as_view(), name="index"),
     path("home.html", views.Home.as_view(), name="index"),
-    path("post_detail/<str:page_type>/<int:pk>/", views.PostDetailView.as_view(), name="detail"),
+    path(
+        "post_detail/<str:page_type>/<int:pk>/",
+        views.PostDetailView.as_view(),
+        name="detail",
+    ),
     path("new.html", views.CreateNewPost.as_view(), name="new_post"),
     # path("new_page_post/<int:id>", views.CreateNewPagePost.as_view(), name="new_page_post"),
+    path(
+        "about",
+        TemplateView.as_view(template_name="feed/landing.html"),
+        name="about",
+    ),
 ]
